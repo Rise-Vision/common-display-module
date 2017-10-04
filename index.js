f459a4732d2f3621e56bb21a9847cc2efa493ab6
Tyler Johnson <mail@tyler-johnson.ca>
2017-10-03 20:38:03 -0400

    Provide only common.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var config = require("./config.js"),
-  machineId = require("./machine-id.js"),
-  messaging = require("./messaging.js");
-
-module.exports = {
-  config,
-  machineId,
-  messaging
-};
