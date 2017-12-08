ec1c19d4fb091ad844ee1fa5019eb5dfb2d6e5cc
rodrigopavezi <rodrigopavezi@gmail.com>
2017-12-08 15:47:00 -0200

    Change to also update components on the player manifest.

--- update-module-version.js
+++ update-module-version.js
@@ -6,6 +6,7 @@ const filePath = process.argv[2];
 const moduleName = process.argv[3];
 const version = process.argv[4];
 const pct = process.argv[5] ? Number(process.argv[5]) : null;
+const type = process.argv[6] ? process.argv[6] : "module";
 
 if (!filePath || !filePath.endsWith(".json")) {return err();}
 if (!version || !version.startsWith("2")) {return err();}
@@ -13,7 +14,12 @@ if (!moduleName) {return err();}
 
 try {
   let json = JSON.parse(fs.readFileSync(filePath));
-  json.modules = json.modules.map(module=>module.name === moduleName ? Object.assign({}, module, {version}) : module);
+  if(type === "module") {
+    json.modules = json.modules.map(module=>module.name === moduleName ? Object.assign({}, module, {version}) : module);
+  } else if(type === "component") {
+    json.components = json.components.map(component=>component.name === moduleName ? Object.assign({}, component, {version}) : component);
+  }
+
   json.rolloutPct = (pct || pct === 0) ? pct : json.rolloutPct;
   fs.writeFileSync(filePath, JSON.stringify(json, null, 2));
 } catch(e) {
