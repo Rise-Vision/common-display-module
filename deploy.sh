faa49cf7f1c950ddd4f57fe8b7b27b4f9c25b667
Tyler Johnson <mail@tyler-johnson.ca>
2017-12-21 18:06:33 -0500

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,13 @@ VERSION=$(cat version)
 OUTPUTDIR="beta/"
 MANIFESTFILES="display-modules-beta*.json"
 
-if [ "$MODULENAME" = "" ]; exit 1; fi
+if [ "$MODULENAME" = "" ]; then exit 1; fi
 
-if [ $BRANCH = "STABLE" ]; then OUTPUTDIR=""; fi
-if [ $BRANCH = "STABLE" ]; then MANIFESTFILES="display-modules-*.json"; fi
+if [ "$BRANCH" = "STABLE" ]; then OUTPUTDIR=""; fi
+if [ "$BRANCH" = "STABLE" ]; then MANIFESTFILES="display-modules-*.json"; fi
 
 echo "deploying $VERSION"
+echo "deploying to install-versions.risevision.com/$OUTPUTDIR"
 
 git clone git@github.com:Rise-Vision/private-keys.git
 gcloud auth activate-service-account 452091732215@developer.gserviceaccount.com --key-file ./private-keys/storage-server/rva-media-library-ce0d2bd78b54.json
@@ -24,7 +25,7 @@ gsutil acl ch -u AllUsers:R gs://install-versions.risevision.com/staging/$MODULE
 gsutil -m cp -p gs://install-versions.risevision.com/${OUTPUTDIR}*.{sh,exe,json} gs://install-versions.risevision.com/backups/$VERSION
 gsutil -m cp -p gs://install-versions.risevision.com/staging/$MODULENAME/$VERSION/* gs://install-versions.risevision.com/$OUTPUTDIR
 
-if [ $BRANCH == "STABLE" ]
+if [ "$BRANCH" = "STABLE" ]
 then
   echo -n "RisePlayerElectron $VERSION" > latest-version
   gsutil cp latest-version gs://install-versions.risevision.com
